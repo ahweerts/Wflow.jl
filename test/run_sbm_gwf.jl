48167ae5bc761bda1819afe65a8db49d885debcb
verseve <willem.vanverseveld@gmail.com>
2022-09-06 14:38:56 +0200

    impoved root water uptake (transpiration) - soil water pressure heads of the root water uptake  (rwu) reduction function (Feddes) can be provided (netCDF) - potential transpiration is partioned over depth using the root fraction - h1 of the rwu reduction function can be provided, either as 1.0 (rwu not constrained during saturated conditions) or 0.0 (rwu constrained during saturated conditions)

--- test/run_sbm_gwf.jl
+++ test/run_sbm_gwf.jl
@@ -15,7 +15,7 @@ flush(model.writer.csv_io)  # ensure the buffer is written fully to disk
 
     @test row.time == DateTime("2000-06-01T00:00:00")
     @test row.Q_av ≈ 0.01620324716944374f0
-    @test row.head ≈ 1.8416896245327632f0
+    @test row.head ≈ 1.844048098862149f0
 end
 
 @testset "first timestep" begin
@@ -25,7 +25,7 @@ end
     @test sbm.θₛ[1] ≈ 0.44999998807907104f0
     @test sbm.runoff[1] == 0.0
     @test sbm.soilevap[1] == 0.0
-    @test sbm.transpiration[1] ≈ 0.6117526566330049f0
+    @test sbm.transpiration[1] ≈ 0.4078351044220031f0
 end
 
 # run the second timestep
@@ -37,7 +37,7 @@ model = Wflow.update(model)
     @test sbm.θₛ[1] ≈ 0.44999998807907104f0
     @test sbm.runoff[1] == 0.0
     @test sbm.soilevap[1] == 0.0
-    @test sbm.transpiration[1] ≈ 1.0122634204681036f0
+    @test sbm.transpiration[1] ≈ 0.6643074006526548f0
 end
 
 @testset "overland flow" begin
